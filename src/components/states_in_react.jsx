f9c5106171262a1bfddcbd05c6c84e225118f629
unknown <upendara628@gmail.com>
2026-01-06 10:21:48 -0800

    heloo

--- src/components/states_in_react.jsx
+++ src/components/states_in_react.jsx
@@ -10,6 +10,16 @@ function StatesDemo(){
         <h2>nmae of the video :{video}</h2>
         <h3>like :{like}</h3>
         <button onClick={() =>(setlike(like+1))}>like</button>
+        <button onClick={()=>(setlike(like-1))}>dislike</button>
+        {
+            // like >=100 &&
+            //    <p>yap you got 100 like </p>
+            like <100?
+            <p>you are the below the 100 likes </p>:
+            <p><b>wow </b>people are liking the your video</p>
+        }
+     
+
         </>
     )
 }
